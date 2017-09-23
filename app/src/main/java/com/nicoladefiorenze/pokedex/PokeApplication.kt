eae7ff0f1987a11d16f95affa52b82458a249aa2
Nicola De Fiorenze <nicola.defiorenze@ennova-research.com>
2017-09-23 10:23:24 +0200

    first import

---
+++ app/src/main/java/com/nicoladefiorenze/pokedex/PokeApplication.kt
@@ -0,0 +1,20 @@
+package com.nicoladefiorenze.pokedex
+
+import android.app.Application
+
+/**
+ * <br/>
+ * Project: Pokedex<br/>
+ * <br/>
+ * created on: 2017-09-22
+ *
+ * @author Nicola De Fiorenze
+ */
+
+class PokeApplication: Application(){
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+}
