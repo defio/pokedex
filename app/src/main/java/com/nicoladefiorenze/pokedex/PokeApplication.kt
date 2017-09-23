b1d0ef820157593f65dc590eabdb263ad66a33c3
Nicola De Fiorenze <nicola.defiorenze@ennova-research.com>
2017-09-23 11:08:24 +0200

    update header file rule

--- app/src/main/java/com/nicoladefiorenze/pokedex/PokeApplication.kt
+++ app/src/main/java/com/nicoladefiorenze/pokedex/PokeApplication.kt
@@ -18,15 +18,6 @@ package com.nicoladefiorenze.pokedex
 
 import android.app.Application
 
-/**
- * <br/>
- * Project: Pokedex<br/>
- * <br/>
- * created on: 2017-09-22
- *
- * @author Nicola De Fiorenze
- */
-
 class PokeApplication: Application(){
 
     override fun onCreate() {
