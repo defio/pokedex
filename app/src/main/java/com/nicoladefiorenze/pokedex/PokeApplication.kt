237cd3077f5357806ab4276ab3d4cab5c099d7b0
Nicola De Fiorenze <nicola.defiorenze@ennova-research.com>
2017-09-23 12:20:39 +0200

    add LeakCanary analyzer

--- app/src/main/java/com/nicoladefiorenze/pokedex/PokeApplication.kt
+++ app/src/main/java/com/nicoladefiorenze/pokedex/PokeApplication.kt
@@ -16,11 +16,26 @@
 package com.nicoladefiorenze.pokedex
 
 import android.app.Application
+import com.squareup.leakcanary.LeakCanary
+import timber.log.Timber
 
 class PokeApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return
+        }
+
+        LeakCanary.install(this)
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+
     }
 
 }
